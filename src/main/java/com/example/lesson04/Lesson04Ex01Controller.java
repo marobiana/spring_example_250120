95fbe9cb4f1b5c6b80aa037853c8476c8f9a1315
test <test@test.com>
2025-04-30 20:16:59 +0900

    모델을 사용한 4강 예제 1번 완료

--- src/main/java/com/example/lesson04/Lesson04Ex01Controller.java
+++ src/main/java/com/example/lesson04/Lesson04Ex01Controller.java
@@ -2,11 +2,14 @@ package com.example.lesson04;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.lesson04.domain.User;
 import com.example.lesson04.service.UserBO;
 
 @RequestMapping("/lesson04/ex01")
@@ -35,6 +38,21 @@ public class Lesson04Ex01Controller {
 		userBO.addUser(name, yyyymmdd, email, introduce);
 		
 		return "lesson04/afterAddUser";
+	}
+	
+	// 최근에 가입한 한명의 유저 정보 화면
+	// http://localhost/lesson04/ex01/latest-user-view
+	@GetMapping("/latest-user-view")
+	public String latestUserView(Model model) {
+		// db select
+		User user = userBO.getLatestUser();
+		
+		// model에 데이터 담아두기 => view가 model에서 꺼내 사용
+		model.addAttribute("result", user);
+		model.addAttribute("title", "방금 가입한 유저 정보");
+		
+		// 화면
+		return "lesson04/latestUser";
 	}
 }
 
--- src/main/java/com/example/lesson04/service/UserBO.java
+++ src/main/java/com/example/lesson04/service/UserBO.java
@@ -3,6 +3,7 @@ package com.example.lesson04.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.lesson04.domain.User;
 import com.example.lesson04.mapper.UserMapper;
 
 @Service
@@ -18,4 +19,14 @@ public class UserBO {
 		
 		userMapper.insertUser(name, yyyymmdd, email, introduce);
 	}
+	
+	// input: X
+	// output: User(단건 or null)
+	public User getLatestUser() {
+		return userMapper.selectLatestUser();
+	}
 }
+
+
+
+
