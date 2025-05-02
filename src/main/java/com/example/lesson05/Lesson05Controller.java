8f6c1dbe3029fabd547995d48827ac161993188d
test <test@test.com>
2025-05-02 21:34:40 +0900

    5강 예제2 반복문

--- src/main/java/com/example/lesson05/Lesson05Controller.java
+++ src/main/java/com/example/lesson05/Lesson05Controller.java
@@ -1,6 +1,12 @@
 package com.example.lesson05;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +17,37 @@ public class Lesson05Controller {
 	@GetMapping("/ex01")
 	public String ex01() {
 		return "lesson05/ex01";
+	}
+	
+	@GetMapping("/ex02")
+	public String ex02(Model model) {
+		// List<String>
+		List<String> fruits = new ArrayList<>();
+		fruits.add("apple");
+		fruits.add("melon");
+		fruits.add("peach");
+		fruits.add("grape");
+		fruits.add("mango");
+		
+		model.addAttribute("fruits", fruits);
+		
+		// List<Map>
+		List<Map<String, Object>> users = new ArrayList<>();
+		Map<String, Object> user = new HashMap<>();
+		user.put("name", "신보람");
+		user.put("age", 30);
+		user.put("hobby", "영화보기");
+		users.add(user);
+
+		user = new HashMap<>();
+		user.put("name", "김바다");
+		user.put("age", 6);
+		user.put("hobby", "달리기");
+		users.add(user);
+		
+		model.addAttribute("users", users); // List!!!!!
+		
+		return "lesson05/ex02";
 	}
 }
 
